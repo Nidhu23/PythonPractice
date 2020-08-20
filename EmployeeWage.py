90134c3b2cf0f79e3dea6394ed406da287703f71
“Nidhu23” <nidhunanik@gmail.com>
2020-08-21 00:09:46 +0530

    [Nidhuna]Add: If-else condition to print employee present or absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,8 @@
+import random
+
 print("Welcome to employee wage problem")
+employeeAttendance = random.randint(0, 1);
+if employeeAttendance == 1:
+    print("Employee present")
+else:
+    print("Employee absent")
