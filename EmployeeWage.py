64e71a0a6209e5218108feaf9a63627d7aacb00f
“Nidhu23” <nidhunanik@gmail.com>
2020-08-21 02:54:17 +0530

    [Nidhuna]Refactor: Code moved to class employee

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,28 +2,44 @@ import random
 
 print("Welcome to employee wage problem")
 
-RATE_PER_HOUR = 20
-FULL_TIME = 1
-PART_TIME = 2
+
+class Employee:
+    FULL_TIME = 1
+    PART_TIME = 2
+
+    def __init__(self, comp_name, max_hrs, max_days, rate_per_hr):
+        self.rate_per_hr = rate_per_hr
+        self.max_days = max_days
+        self.max_hrs = max_hrs
+        self.COMP_NAME = comp_name
+
+    def switch(attendance):
+        return {
+            Employee.FULL_TIME: 8,
+            Employee.PART_TIME: 4
+        }.get(attendance, 0)
+
+    def compute_monthly_wage(self):
+        total_days = 0
+        emp_hrs = 0
+        while total_days < self.max_days and emp_hrs < self.max_hrs:
+            total_days += 1
+            employee_attendance = random.randint(0, 2)
+            emp_hrs = emp_hrs + Employee.switch(employee_attendance)
+        print("Monthly Wage of employee in " + self.COMP_NAME, emp_hrs * self.rate_per_hr)
 
 
-def switch(attendance):
-    return {
-        FULL_TIME: 8,
-        PART_TIME: 4
-    }.get(attendance, 0)
+def main():
+    n = 1
+    while n != 0:
+        comp_name = input("Enter company name ")
+        max_hours = int(input("Enter max working hours "))
+        max_days = int(input("Enter max working days "))
+        rate_per_hr = int(input("Enter rate per hour "))
+        employee = Employee(comp_name, max_hours, max_days, rate_per_hr)
+        employee.compute_monthly_wage()
+        print("Enter 1 to continue, 0 to quit")
+        n = int(input())
 
 
-def compute_monthly_wage():
-    total_days = 0
-    emp_hrs = 0
-    MAX_WORKING_DAYS = 20
-    MAX_WORKING_HRS = 100
-    while total_days < MAX_WORKING_DAYS and emp_hrs < MAX_WORKING_HRS:
-        total_days += 1
-        employee_attendance = random.randint(0, 2)
-        emp_hrs = emp_hrs + switch(employee_attendance)
-    return emp_hrs
-
-
-print("Monthly Wage = ", compute_monthly_wage() * RATE_PER_HOUR)
+main()
